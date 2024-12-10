ef09c6f84474c03d855c587d4fea70a63cf8ea9b
Tedd Lee <tedd0328@gmail.com>
2024-12-09 22:20:19 -0500

    Update Streamlit App

--- StreamlitCrisis/model.py
+++ StreamlitCrisis/model.py
@@ -6,8 +6,8 @@ model = CatBoostClassifier()
 model.load_model('model_v0')
 
 target_col = 'FINAL_CALL_TYPE'
-feature_cols = ['INITIAL_CALL_TYPE', 'INITIAL_SEVERITY_LEVEL_CODE', 'day_of_week', 'incident_hour', 'incident_duration', 'POLICEPRECINCT', 'ZIPCODE']
-categorical_features = ['INITIAL_CALL_TYPE', 'day_of_week', 'POLICEPRECINCT', 'ZIPCODE']
+feature_cols = ['INITIAL_CALL_TYPE', 'INITIAL_SEVERITY_LEVEL_CODE', 'DAY_OF_WEEK', 'INCIDENT_HOUR', 'INCIDENT_DURATION', 'POLICEPRECINCT', 'ZIPCODE']
+categorical_features = ['INITIAL_CALL_TYPE', 'DAY_OF_WEEK', 'POLICEPRECINCT', 'ZIPCODE']
 
 def encode(data):
 	params = data.copy()
@@ -15,9 +15,9 @@ def encode(data):
 	params['INCIDENT_DATETIME'] = pd.to_datetime(params['INCIDENT_DATETIME'])
 	params['INCIDENT_CLOSE_DATETIME'] = pd.to_datetime(params['INCIDENT_CLOSE_DATETIME'])
 
-	params['day_of_week'] = params['INCIDENT_DATETIME'].dayofweek
-	params['incident_hour'] = params['INCIDENT_DATETIME'].hour
-	params['incident_duration'] = (params['INCIDENT_CLOSE_DATETIME'] - params['INCIDENT_DATETIME']).total_seconds()
+	params['DAY_OF_WEEK'] = params['INCIDENT_DATETIME'].dayofweek
+	params['INCIDENT_HOUR'] = params['INCIDENT_DATETIME'].hour
+	params['INCIDENT_DURATION'] = (params['INCIDENT_CLOSE_DATETIME'] - params['INCIDENT_DATETIME']).total_seconds()
 	params['POLICEPRECINCT'] = params['POLICEPRECINCT'].astype(str)
 	params['ZIPCODE'] = params['ZIPCODE'].astype(str)
 
@@ -26,6 +26,7 @@ def encode(data):
 
 def predict(params):
 	try:
+		print(params)
 		res = model.predict(params)
 
 		return res[0]
